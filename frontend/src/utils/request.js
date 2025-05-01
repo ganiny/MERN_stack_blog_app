5aa70da855027b10a923ef8dd0c1084dd8e853bd
Mohamed Abubakr <113220329+ganiny@users.noreply.github.com>
2025-05-01 13:24:05 +0300

    add vercel.json

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://mern-stack-blog-app-37wy.onrender.com",
 });
 
 export default request;
