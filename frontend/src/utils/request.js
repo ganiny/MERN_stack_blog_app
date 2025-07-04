2f9b0049e45eb30b330f95ffbe8c7e2901159698
Mohamed Abubakr <113220329+ganiny@users.noreply.github.com>
2025-07-05 02:31:56 +0300

    Update request.js

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://mern-stack-blog-app-1bhv.vercel.app",
 });
 
 export default request;
